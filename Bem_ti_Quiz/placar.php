8eadfe50bbc9b9c104f2c0a67223e32af43cadc4
igormassami9 <107521106+igormassami9@users.noreply.github.com>
2023-10-16 19:09:56 -0300

    placar

---
+++ Bem_ti_Quiz/placar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Placar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="Css/Style.css" rel="stylesheet" />
+    <style>
+        body {
+            background-image: url('Css/BGLOGO.png');
+            background-size: cover;
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+        .quadro {
+            width: 801px;
+            height: 435px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            border-radius: 20px;
+            border: 2px solid #777;
+            background: rgba(255, 255, 255, 0.88);
+            box-shadow: 0px 8px 0px 0px #777, 4px 13px 4px 0px rgba(0, 0, 0, 0.25);
+        }
+    </style>
+</head>
+<body>
+<div class="rodape">
+        <h1 class="texto-rodape">Placar</h1>
+    </div>
+    <div class="quadro">
+        <div class="rodape-placar">
+            <h1>Seu placar</h1>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
+
+
